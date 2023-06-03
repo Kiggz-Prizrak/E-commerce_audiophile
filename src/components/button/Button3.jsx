77e6263a00693bb4ba89a2745b1dc048a7351c0d
Kiggz <nico.peignot@outlook.fr>
2023-06-03 16:26:42 +0200

    home category link patch

--- src/components/button/Button3.jsx
+++ src/components/button/Button3.jsx
@@ -2,10 +2,11 @@ import { NavLink } from "react-router-dom";
 
 import OrangeArrrow from "../../assets/icons/OrangeArrow";
 
-const Button3 = (link) => {
+const Button3 = ({link}) => {
+  console.log(link);
   return (
     <button className="Button3">
-      <NavLink to={link}>SHOP</NavLink>
+      <NavLink to={`/${link}`}>SHOP</NavLink>
       <OrangeArrrow />
     </button>
   );
