b234cb329274a8d89815fdb3341d0b1590da22ae
Kiggz <nico.peignot@outlook.fr>
2023-06-08 00:29:25 +0200

    redux store cart configuration

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import router from "./router";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
+import { store } from "./store";
+import { Provider } from "react-redux";
 import "./stylesheets/main.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <RouterProvider router={router} />
+    </React.StrictMode>
+  </Provider>
 );
---
+++ src/store/cartSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    cart: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const itemInCart = state.cart.find(
+        (item) => item.id === action.payload.id
+      );
+      if (itemInCart) {
+        itemInCart.productQuantity += action.payload.productQuantity;
+      } else {
+        state.cart.push({ ...action.payload });
+      }
+    },
+
+    incrementQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload);
+      item.productQuantity++;
+    },
+
+    decrementQuantity: (state, action) => {
+      const item = state.cart.find((item) => item.id === action.payload);
+      item.productQuantity === 1
+        ? (item.productQuantity = 1)
+        : item.productQuantity--;
+    },
+
+    removeItem: (state, action) => {
+      const removeItem = state.cart.filter(
+        (item) => item.id !== action.payload
+      );
+      state.cart = removeItem;
+    },
+  },
+});
+
+export const cartReducer = cartSlice.reducer;
+
+export const { addToCart, incrementQuantity, decrementQuantity, removeItem } =
+  cartSlice.actions;
